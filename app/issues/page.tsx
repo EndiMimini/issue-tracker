b338d291d1d412340c6820fc8bbd8826a9a78e3a
EndiMimini <3ndi.mimini@gmail.com>
2024-06-04 13:19:00 +0200

    handle form submission

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Button } from '@radix-ui/themes'
 import Link from 'next/link'
+import axios from 'axios'
 const IssuesPage = () => {
+
   return (
     <div>
       <Button><Link href='/issues/new'>New Issue</Link></Button>
